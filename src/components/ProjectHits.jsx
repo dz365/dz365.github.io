a8b50991d564b93a7610e71d9242db32565b8f1b
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2023-05-21 12:07:20 -0400

    Refactored work and projects to own file

---
+++ src/components/ProjectHits.jsx
@@ -0,0 +1,39 @@
+import { ASTEROIDS, ECOSQUAD, WEBGALLERY } from "../constants/Projects";
+import BubbleList from "./BubbleList";
+import SearchHit from "./SearchHit";
+
+const ProjectHits = () => {
+  return (
+    <>
+      <SearchHit
+        image={ECOSQUAD.logo}
+        pageTitle={ECOSQUAD.name}
+        pageLink={ECOSQUAD.url}
+        hitTitle={ECOSQUAD.name}
+        date={ECOSQUAD.duration}
+        description={ECOSQUAD.description}
+        additionalInfo={<BubbleList list={ECOSQUAD.technologies} />}
+      />
+      <SearchHit
+        image={WEBGALLERY.logo}
+        pageTitle={WEBGALLERY.name}
+        pageLink={WEBGALLERY.url}
+        hitTitle={WEBGALLERY.name}
+        date={WEBGALLERY.duration}
+        description={WEBGALLERY.description}
+        additionalInfo={<BubbleList list={WEBGALLERY.technologies} />}
+      />
+      <SearchHit
+        image={ASTEROIDS.logo}
+        pageTitle={ASTEROIDS.name}
+        pageLink={ASTEROIDS.url}
+        hitTitle={ASTEROIDS.name}
+        date={ASTEROIDS.duration}
+        description={ASTEROIDS.description}
+        additionalInfo={<BubbleList list={ASTEROIDS.technologies} />}
+      />
+    </>
+  );
+};
+
+export default ProjectHits;
---
+++ src/components/WorkExperienceHits.jsx
@@ -0,0 +1,30 @@
+import { CASEWARE, SKATESCRIBE } from "../constants/WorkExperiences";
+import BubbleList from "./BubbleList";
+import SearchHit from "./SearchHit";
+
+const WorkExperienceHits = () => {
+  return (
+    <>
+      <SearchHit
+        image={CASEWARE.logo}
+        pageTitle={CASEWARE.company}
+        pageLink={CASEWARE.url}
+        hitTitle={CASEWARE.jobTitle}
+        date={CASEWARE.duration}
+        description={CASEWARE.description}
+        additionalInfo={<BubbleList list={CASEWARE.technologies} />}
+      />
+      <SearchHit
+        image={SKATESCRIBE.logo}
+        pageTitle={SKATESCRIBE.company}
+        pageLink={SKATESCRIBE.url}
+        hitTitle={SKATESCRIBE.jobTitle}
+        date={SKATESCRIBE.duration}
+        description={SKATESCRIBE.description}
+        additionalInfo={<BubbleList list={SKATESCRIBE.technologies} />}
+      />
+    </>
+  );
+};
+
+export default WorkExperienceHits;

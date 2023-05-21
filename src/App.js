fb389b2dca0844e207271402d0659dfe741bbe1e
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2023-05-21 11:20:35 -0400

    Moved search hit to own component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import { ReactComponent as GridIcon } from "./assets/icons/grid.svg";
 import { ReactComponent as SearchIcon } from "./assets/icons/search.svg";
 import { ReactComponent as XMarkIcon } from "./assets/icons/xmark.svg";
 import catImage from "./assets/images/cat.webp";
-import casewareImage from "./assets/images/caseware.webp";
-import skatescribeImage from "./assets/images/skatescribe.webp";
 import ecosquadImage from "./assets/images/ecosquad.webp";
 import webGalleryImage from "./assets/images/webgallery.webp";
 import asteroidsImage from "./assets/images/asteroids.webp";
 import resume from "./assets/Daniel_Zhang_Resume.pdf";
 import googleLight from "./assets/images/google-light.png";
+import BubbleList from "./components/BubbleList";
+import SearchHit from "./components/SearchHit";
+import { CASEWARE, SKATESCRIBE } from "./constants/WorkExperience";
 function App() {
+  const TECHNOLOGIES = {
+    skatescribe: [
+      "Angular",
+      "Python",
+      "Python Flask",
+      "SocketIO",
+      "PostgreSQL",
+    ],
+    ecosquad: [
+      "React",
+      "MapLibre GL",
+      "ExpressJS",
+      "PostgreSQL",
+      "Meilisearch",
+      "Docker",
+    ],
+    webgallery: ["HTML", "CSS", "Javascript", "ExpressJS", "SQLite"],
+    asteroids: ["Java", "Java Swing"],
+  };
   const [showOptions, setShowOptions] = useState(false);
 
   const divRef = useRef(null);
@@ -37,7 +57,7 @@ function App() {
   return (
     <div className="w-full h-screen">
       <div className="sticky top-0 left-0 bg-white border-b px-5 py-3 w-full flex items-center">
-        <div className="w-full flex md:flex-row flex-col items-center gap-2 md:gap-8">
+        <div className="w-full flex lg:flex-row flex-col items-center gap-2 lg:gap-8">
           <img alt="Google" src={googleLight} className="w-24" />
           <div className="w-full max-w-lg border shadow-md rounded-3xl px-4 py-2.5 flex justify-between gap-3">
             <span className="text-gray-700 pr-2">daniel zhang website</span>
@@ -62,103 +82,31 @@ function App() {
           </div>
         </button>
       </div>
-      <div className="md:px-52 px-2 py-4 w-full md:w-10/12 flex flex-col-reverse md:flex-row md:items-start gap-16">
-        <div className="md:w-5/12 flex flex-col  md:basis-4/6">
+      <div className="lg:px-52 px-2 py-4 w-full lg:w-10/12 flex flex-col-reverse lg:flex-row lg:items-start gap-16">
+        <div className="lg:w-5/12 flex flex-col  lg:basis-4/6">
           <span className="text-sm text-gray-700">
             5 results (0.01 seconds)
           </span>
           <span className="text-2xl text-blue-900 my-4">Work Experience</span>
           <div className="flex flex-col gap-10">
-            <div className="flex flex-col gap-3">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                  <img
-                    src={casewareImage}
-                    className="w-8 h-8 bg-google-gray rounded-full p-1"
-                    alt="work"
-                  />
-                  <div className="flex flex-col">
-                    <span>Caseware</span>
-                    <span className="text-sm text-gray-700">
-                      https://www.caseware.com
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col md:flex-row md:items-center justify-between">
-                <span className="text-xl text-violet-700">
-                  Software Developer
-                </span>
-                <span className="text-gray-700">
-                  January 2022 - August 2022
-                </span>
-              </div>
-
-              <p className="text-gray-800">
-                My second internship was for 8 months at a medium sized auditing
-                company called Caseware. At the time I was working, Caseware was
-                in a transitional stage to fully move all their products to
-                cloud providers. As such, most of what I worked on were related
-                to this. Some of the major things I did include development of a
-                routing microservice to handle incoming requests, and system
-                improvements to better support client needs.
-              </p>
-              <div className="flex gap-2 flex-wrap	">
-                {["Java", "Spring Boot", "AWS DynamoDB", "Redis", "C++"].map(
-                  (language) => (
-                    <span className="bg-google-gray px-3 py-1 rounded-3xl text-sm">
-                      {language}
-                    </span>
-                  )
-                )}
-              </div>
-            </div>
-            <div className="flex flex-col gap-3">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                  <img
-                    src={skatescribeImage}
-                    className="w-8 h-8 bg-google-gray rounded-full p-1"
-                    alt="work"
-                  />
-                  <div className="flex flex-col">
-                    <span>Skatescribe</span>
-                    <span className="text-sm text-gray-700">
-                      https://skatescribe.com
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col md:flex-row md:items-center justify-between">
-                <span className="text-xl text-violet-700">
-                  Full Stack Developer
-                </span>
-                <span className="text-gray-700">May 2021 - August 2021</span>
-              </div>
-
-              <p className="text-gray-900">
-                My first internship was for 4 months at a startup called
-                Skatescribe. Backed by multiple NHL investors, Skatescribe is
-                revolutionizing the skate sharpening industry by using machines,
-                leading to high quality cuts that cannot be replicated anywhere
-                else. What I worked on during my time here was developing
-                measures to improve operational safety and automation to reduce
-                the amount of interaction an operator has with the software.
-              </p>
-              <div className="flex gap-2 flex-wrap	">
-                {[
-                  "Angular",
-                  "Python",
-                  "Python Flask",
-                  "SocketIO",
-                  "PostgreSQL",
-                ].map((language) => (
-                  <span className="bg-google-gray px-3 py-1 rounded-3xl text-sm">
-                    {language}
-                  </span>
-                ))}
-              </div>
-            </div>
+            <SearchHit
+              image={CASEWARE.logo}
+              pageTitle={CASEWARE.company}
+              pageLink={CASEWARE.url}
+              hitTitle={CASEWARE.jobTitle}
+              date={CASEWARE.duration}
+              description={CASEWARE.description}
+              additionalInfo={<BubbleList list={CASEWARE.technologies} />}
+            />
+            <SearchHit
+              image={SKATESCRIBE.logo}
+              pageTitle={SKATESCRIBE.company}
+              pageLink={SKATESCRIBE.url}
+              hitTitle={SKATESCRIBE.jobTitle}
+              date={SKATESCRIBE.duration}
+              description={SKATESCRIBE.description}
+              additionalInfo={<BubbleList list={SKATESCRIBE.technologies} />}
+            />
           </div>
           <span className="text-2xl text-blue-900 my-4">Projects</span>
           <div className="flex flex-col gap-10">
@@ -186,19 +134,7 @@ function App() {
                 time. Hence we made a citizen scientist app where users could
                 share, search, and view discoveries on an interactive map.
               </p>
-              <div className="flex gap-2 flex-wrap	">
-                {[
-                  "React",
-                  "MapLibre GL",
-                  "ExpressJS",
-                  "PostgreSQL",
-                  "Meilisearch",
-                ].map((language) => (
-                  <span className="bg-google-gray px-3 py-1 rounded-3xl text-sm">
-                    {language}
-                  </span>
-                ))}
-              </div>
+              <BubbleList list={TECHNOLOGIES.ecosquad} />
             </div>
             <div className="flex flex-col gap-3">
               <div className="flex items-center justify-between">
@@ -224,15 +160,7 @@ function App() {
                 like authentication and authorization, pagination were also
                 implemented.
               </p>
-              <div className="flex gap-2 flex-wrap	">
-                {["HTML", "CSS", "Javascript", "ExpressJS", "SQLite"].map(
-                  (language) => (
-                    <span className="bg-google-gray px-3 py-1 rounded-3xl text-sm">
-                      {language}
-                    </span>
-                  )
-                )}
-              </div>
+              <BubbleList list={TECHNOLOGIES.webgallery} />
             </div>
             <div className="flex flex-col gap-3">
               <div className="flex items-center justify-between">
@@ -259,17 +187,11 @@ function App() {
                 of Asteroids by Altari, alongisde additional features like
                 powerups.
               </p>
-              <div className="flex gap-2 flex-wrap	">
-                {["Java", "Java Swing"].map((language) => (
-                  <span className="bg-google-gray px-3 py-1 rounded-3xl text-sm">
-                    {language}
-                  </span>
-                ))}
-              </div>
+              <BubbleList list={TECHNOLOGIES.asteroids} />
             </div>
           </div>
         </div>
-        <div className="border rounded-lg p-4 flex flex-col gap-4 md:basis-2/6">
+        <div className="border rounded-lg p-4 flex flex-col gap-4 lg:basis-2/6">
           <div className="flex items-center justify-between items-center">
             <div className="basis-7/12 flex flex-col">
               <span className="text-3xl">Daniel Zhang</span>
@@ -342,10 +264,6 @@ function App() {
           <button className="p-2 hover:bg-blue-gray rounded-xl flex flex-col items-center justify-center gap-2">
             <div className="w-14 h-14 bg-contain bg-center bg-[url('./assets/images/programing.webp')]"></div>
             <span>Projects</span>
-          </button>
-          <button className="p-2 hover:bg-blue-gray rounded-xl flex flex-col items-center justify-center gap-2">
-            <div className="w-14 h-14 bg-contain bg-center bg-[url('./assets/images/docs.webp')]"></div>
-            <span>Resume</span>
           </button>
         </div>
       )}
---
+++ src/constants/WorkExperience.js
@@ -0,0 +1,33 @@
+import casewareImage from "../assets/images/caseware.webp";
+import skatescribeImage from "../assets/images/skatescribe.webp";
+
+const CASEWARE = {
+  company: "Caseware",
+  logo: casewareImage,
+  url: "https://www.caseware.com",
+  jobTitle: "Software Developer",
+  duration: "January 2022 - August 2022",
+  description: `My second internship was for 8 months at a medium sized auditing company called Caseware. 
+    At the time I was working, Caseware was in a transitional stage to fully move all their products to cloud providers. 
+    As such, most of what I worked on were related to this. Some of the major things I did include development of a 
+    routing microservice to handle incoming requests, and system improvements to better support client needs.`,
+  technologies: ["Java", "Spring Boot", "AWS DynamoDB", "Redis", "C++"],
+};
+
+const SKATESCRIBE = {
+  company: "Skatescribe",
+  logo: skatescribeImage,
+  url: "https://skatescribe.com",
+  jobTitle: "Full Stack Developer",
+  duration: "May 2021 - August 2021",
+  description: `My first internship was for 4 months at a startup called
+    Skatescribe. Backed by NHL investors, they are revolutionizing 
+    the skate sharpening industry by using machines,
+    leading to high quality cuts that cannot be replicated anywhere
+    else. What I worked on during my time here was developing
+    measures to improve operational safety and automation to reduce
+    the amount of interaction an operator has with the software.`,
+  technologies: ["Angular", "Python", "Python Flask", "SocketIO", "PostgreSQL"],
+};
+
+export { CASEWARE, SKATESCRIBE };

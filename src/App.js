7f065cdabd7b39469c1fa9338b909c248ce91120
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2023-05-21 11:38:02 -0400

    Refactored code to use SearchHits

--- src/App.js
+++ src/App.js
@@ -3,34 +3,13 @@ import { ReactComponent as GridIcon } from "./assets/icons/grid.svg";
 import { ReactComponent as SearchIcon } from "./assets/icons/search.svg";
 import { ReactComponent as XMarkIcon } from "./assets/icons/xmark.svg";
 import catImage from "./assets/images/cat.webp";
-import ecosquadImage from "./assets/images/ecosquad.webp";
-import webGalleryImage from "./assets/images/webgallery.webp";
-import asteroidsImage from "./assets/images/asteroids.webp";
 import resume from "./assets/Daniel_Zhang_Resume.pdf";
 import googleLight from "./assets/images/google-light.png";
 import BubbleList from "./components/BubbleList";
 import SearchHit from "./components/SearchHit";
-import { CASEWARE, SKATESCRIBE } from "./constants/WorkExperience";
+import { CASEWARE, SKATESCRIBE } from "./constants/WorkExperiences";
+import { ASTEROIDS, ECOSQUAD, WEBGALLERY } from "./constants/Projects";
 function App() {
-  const TECHNOLOGIES = {
-    skatescribe: [
-      "Angular",
-      "Python",
-      "Python Flask",
-      "SocketIO",
-      "PostgreSQL",
-    ],
-    ecosquad: [
-      "React",
-      "MapLibre GL",
-      "ExpressJS",
-      "PostgreSQL",
-      "Meilisearch",
-      "Docker",
-    ],
-    webgallery: ["HTML", "CSS", "Javascript", "ExpressJS", "SQLite"],
-    asteroids: ["Java", "Java Swing"],
-  };
   const [showOptions, setShowOptions] = useState(false);
 
   const divRef = useRef(null);
@@ -110,85 +89,33 @@ function App() {
           </div>
           <span className="text-2xl text-blue-900 my-4">Projects</span>
           <div className="flex flex-col gap-10">
-            <div className="flex flex-col gap-3">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                  <img
-                    src={ecosquadImage}
-                    className="w-8 h-8 bg-google-gray rounded-full p-1"
-                    alt="work"
-                  />
-                  <div className="flex flex-col">
-                    <span>Ecosquad</span>
-                    <span className="text-sm text-gray-700">
-                      https://github.com/dz365/project-ecosquad
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <span className="text-xl text-violet-700">Ecosquad</span>
-              <p className="text-gray-900">
-                Ecosquad is a web app developed by me and a friend of mine. The
-                inspiration behind it was that we wanted a place where we could
-                upload wildlife discoveries that we would make from time to
-                time. Hence we made a citizen scientist app where users could
-                share, search, and view discoveries on an interactive map.
-              </p>
-              <BubbleList list={TECHNOLOGIES.ecosquad} />
-            </div>
-            <div className="flex flex-col gap-3">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                  <img
-                    src={webGalleryImage}
-                    className="w-8 h-8 bg-google-gray rounded-full p-1"
-                    alt="work"
-                  />
-                  <div className="flex flex-col">
-                    <span>Web Gallery</span>
-                    <span className="text-sm text-gray-700">
-                      https://github.com/dz365/Web-Gallery
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <span className="text-xl text-violet-700">Web Gallery</span>
-              <p className="text-gray-900">
-                Web Gallery is a web app that allows people to upload and view
-                photos and attach comments. Ultimately, this app was just to
-                test my understanding of the basics of web programming. Features
-                like authentication and authorization, pagination were also
-                implemented.
-              </p>
-              <BubbleList list={TECHNOLOGIES.webgallery} />
-            </div>
-            <div className="flex flex-col gap-3">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                  <img
-                    src={asteroidsImage}
-                    className="w-8 h-8 bg-google-gray rounded-full p-1"
-                    alt="work"
-                  />
-                  <div className="flex flex-col">
-                    <span>Asteroid Remake</span>
-                    <span className="text-sm text-gray-700">
-                      https://github.com/dz365/Asteroid-Remake
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <span className="text-xl text-violet-700">Asteroid Remake</span>
-              <p className="text-gray-900">
-                This was a project that I did along with a friend quite a while
-                back, but I had so much fun creating that I decided to include
-                it here. It's also nice to be reminded of how far I've come in
-                such a short period of time. As per the name, this was a remake
-                of Asteroids by Altari, alongisde additional features like
-                powerups.
-              </p>
-              <BubbleList list={TECHNOLOGIES.asteroids} />
-            </div>
+            <SearchHit
+              image={ECOSQUAD.logo}
+              pageTitle={ECOSQUAD.name}
+              pageLink={ECOSQUAD.url}
+              hitTitle={ECOSQUAD.name}
+              date={ECOSQUAD.duration}
+              description={ECOSQUAD.description}
+              additionalInfo={<BubbleList list={ECOSQUAD.technologies} />}
+            />
+            <SearchHit
+              image={WEBGALLERY.logo}
+              pageTitle={WEBGALLERY.name}
+              pageLink={WEBGALLERY.url}
+              hitTitle={WEBGALLERY.name}
+              date={WEBGALLERY.duration}
+              description={WEBGALLERY.description}
+              additionalInfo={<BubbleList list={WEBGALLERY.technologies} />}
+            />
+            <SearchHit
+              image={ASTEROIDS.logo}
+              pageTitle={ASTEROIDS.name}
+              pageLink={ASTEROIDS.url}
+              hitTitle={ASTEROIDS.name}
+              date={ASTEROIDS.duration}
+              description={ASTEROIDS.description}
+              additionalInfo={<BubbleList list={ASTEROIDS.technologies} />}
+            />
           </div>
         </div>
         <div className="border rounded-lg p-4 flex flex-col gap-4 lg:basis-2/6">

589df1d2d4fb22f8f8388879ab183fb6078bbadb
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2023-05-21 17:09:27 -0400

    apply dark theme for search hit

--- src/components/SearchHit.jsx
+++ src/components/SearchHit.jsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
 const SearchHit = ({
   image,
   pageTitle,
@@ -7,6 +10,7 @@ const SearchHit = ({
   description,
   additionalInfo,
 }) => {
+  const { theme } = useContext(ThemeContext);
   return (
     <div className="flex flex-col gap-3">
       <div className="flex items-center justify-between cursor-pointer group">
@@ -17,19 +21,29 @@ const SearchHit = ({
             alt={pageTitle}
           />
           <div className="flex flex-col">
-            <span>{pageTitle}</span>
-            <span className="text-sm text-gray-700 group-hover:underline">
+            <span className={theme === "dark" && "text-gray-100"}>
+              {pageTitle}
+            </span>
+            <span
+              className={`text-sm group-hover:underline ${
+                theme === "dark" ? "text-gray-400" : "text-gray-700"
+              }`}
+            >
               {pageLink}
             </span>
           </div>
         </div>
       </div>
       <div className="flex flex-col lg:flex-row lg:items-center justify-between">
-        <span className="text-xl text-violet-700">{hitTitle}</span>
-        <span className="text-gray-700">{date}</span>
+        <span className={`text-xl ${theme === "dark" ? "text-purple-400" : "text-violet-700"}`}>{hitTitle}</span>
+        <span className={theme === "dark" ? "text-gray-400" : "text-gray-800"}>
+          {date}
+        </span>
       </div>
 
-      <p className="text-gray-800">{description}</p>
+      <p className={theme === "dark" ? "text-gray-300" : "text-gray-800"}>
+        {description}
+      </p>
       {additionalInfo}
     </div>
   );

696400c6d6db1d9e5c350db5d96147d82add068b
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2024-05-05 12:30:44 -0400

    Move window dragging to window file

--- window.js
+++ window.js
@@ -1,16 +1,35 @@
+function createTitlebar() {
+  const titlebar = document.createElement("div");
+  titlebar.classList.add("window-titlebar");
+  titlebar.innerHTML = `
+    <div class="window-actions">
+      <button class="close-button"></button>
+      <button class="minimize-button"></button>
+      <button class="expand-button"></button>
+    </div>
+  `;
+  return titlebar;
+}
+
+function addWindowActions(window) {
+  const windowActions = window.querySelector(".window-actions");
+
+  // Trigger the hover effect on mouse enter
+  windowActions.addEventListener("mouseenter", () => {
+    windowActions.querySelectorAll("button").forEach((button) => {
+      button.classList.add("hovered");
+    });
+  });
+
+  // Remove the hover effect on mouse leave
+  windowActions.addEventListener("mouseleave", () => {
+    windowActions.querySelectorAll("button").forEach((button) => {
+      button.classList.remove("hovered");
+    });
+  });
+}
+
 function createWindow(content) {
-  function createTitlebar() {
-    const titlebar = document.createElement("div");
-    titlebar.classList.add("window-titlebar");
-    titlebar.innerHTML = `
-      <div class="window-actions">
-        <button class="close-button"></button>
-        <button class="minimize-button"></button>
-        <button class="expand-button"></button>
-      </div>
-    `;
-    return titlebar;
-  }
   const window = document.createElement("div");
   window.classList.add("window");
   window.appendChild(createTitlebar());
@@ -19,5 +38,10 @@ function createWindow(content) {
   windowContent.classList.add("window-content");
   windowContent.appendChild(content);
   window.appendChild(windowContent);
+
+  const titlebar = window.querySelector(".window-titlebar");
+  makeElementDraggable(window, titlebar);
+
+  addWindowActions(window);
   return window;
 }

2417463bb1c69e8a196ada6c3f95df60d875fce2
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2024-05-05 12:40:33 -0400

    Open resume on dock click

--- dock.js
+++ dock.js
@@ -3,6 +3,12 @@ window.addEventListener("DOMContentLoaded", function () {
     displayReminderAppWindow("All Experiences");
   }
 
+  function handleResumeAppClick() {
+    displayResumeAppWindow()
+  }
   const reminderApp = document.getElementById("reminderApp");
   reminderApp.addEventListener("click", handleReminderAppClick);
+
+  const resumeApp = document.getElementById("resumeApp");
+  resumeApp.addEventListener("click", handleResumeAppClick);
 });

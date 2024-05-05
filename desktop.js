477d7d929da548134cbb5c81b013c28115bc3b06
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2024-05-05 12:30:44 -0400

    Move window dragging to window file

--- desktop.js
+++ desktop.js
@@ -1,38 +1,4 @@
 window.addEventListener("DOMContentLoaded", function () {
-  function makeElementDraggable(windowElement, titlebar) {
-    let offsetX, offsetY;
-
-    function onMouseDown(event) {
-      offsetX = event.clientX - windowElement.getBoundingClientRect().left;
-      offsetY = event.clientY - windowElement.getBoundingClientRect().top;
-      const iframe = windowElement.querySelector("iframe");
-      if (iframe) iframe.style.pointerEvents = "none";
-      document.addEventListener("mousemove", onMouseMove);
-      document.addEventListener("mouseup", onMouseUp);
-    }
-
-    function onMouseMove(event) {
-      let newX = event.clientX - offsetX;
-      let newY = event.clientY - offsetY;
-
-      if (newY < 0) newY = 0;
-      if (newY > window.innerHeight - 20) newY = window.innerHeight - 20;
-      if (newX > window.innerWidth - 20) newX = window.innerWidth - 20;
-
-      windowElement.style.left = newX + "px";
-      windowElement.style.top = newY + "px";
-    }
-
-    function onMouseUp() {
-      document.removeEventListener("mousemove", onMouseMove);
-      document.removeEventListener("mouseup", onMouseUp);
-      const iframe = windowElement.querySelector("iframe");
-      if (iframe) iframe.style.pointerEvents = "auto";
-    }
-
-    titlebar.addEventListener("mousedown", onMouseDown);
-  }
-
   const draggableElements = document.querySelectorAll(".draggable");
   draggableElements.forEach((element) =>
     makeElementDraggable(element, element)
@@ -51,28 +17,6 @@ window.addEventListener("DOMContentLoaded", function () {
 
   const finderTitlebar = document.querySelectorAll(".finder-titlebar");
   finderTitlebar.forEach((titlebar) => makeElementDraggable(finder, titlebar));
-
-  const windows = document.querySelectorAll(".window");
-  windows.forEach((window) => {
-    const titlebar = window.querySelector(".window-titlebar");
-    makeElementDraggable(window, titlebar);
-
-    const windowActions = window.querySelector(".window-actions");
-
-    // Trigger the hover effect on mouse enter
-    windowActions.addEventListener("mouseenter", () => {
-      windowActions.querySelectorAll("button").forEach((button) => {
-        button.classList.add("hovered");
-      });
-    });
-
-    // Remove the hover effect on mouse leave
-    windowActions.addEventListener("mouseleave", () => {
-      windowActions.querySelectorAll("button").forEach((button) => {
-        button.classList.remove("hovered");
-      });
-    });
-  });
 
   const desktopIcons = [
     {

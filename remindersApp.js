77ddc4cf60dc9c217b995e5b35aee1116cac0d27
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2024-05-05 12:22:45 -0400

    Open reminder on dock click

--- remindersApp.js
+++ remindersApp.js
@@ -1,139 +1,135 @@
-window.addEventListener("DOMContentLoaded", function () {
-  const sections = {
-    Career: "Career",
-    Projects: "Projects",
-  };
+const sections = {
+  Career: "Career",
+  Projects: "Projects",
+};
 
-  const sideBarContents = [
-    {
-      sectionTitle: "All Experiences",
-      sectionClass: "all-experiences",
-      sectionCount: 0,
-    },
-    {
-      sectionTitle: sections.Career,
-      sectionClass: "work-experiences",
-      sectionCount: 2,
-    },
-    {
-      sectionTitle: sections.Projects,
-      sectionClass: "projects",
-      sectionCount: 0,
-    },
-  ];
+const sideBarContents = [
+  {
+    sectionTitle: "All Experiences",
+    sectionClass: "all-experiences",
+    sectionCount: 0,
+  },
+  {
+    sectionTitle: sections.Career,
+    sectionClass: "work-experiences",
+    sectionCount: 2,
+  },
+  {
+    sectionTitle: sections.Projects,
+    sectionClass: "projects",
+    sectionCount: 0,
+  },
+];
 
-  const experienceTimeline = [
-    {
-      year: 2023,
-      experiences: [
-        {
-          type: sections.Projects,
-          title: "Ecosquad",
-          description: "A citizen scientist web app",
-          class: "ecosquad",
-        },
-      ],
-    },
-    {
-      year: 2022,
-      experiences: [
-        {
-          type: sections.Career,
-          title: "Caseware",
-          description: "Software Developer Intern",
-          class: "caseware",
-        },
-      ],
-    },
-    {
-      year: 2021,
-      experiences: [
-        {
-          type: sections.Career,
-          title: "Skatescribe",
-          description: "Full Stack Developer Intern",
-          class: "skatescribe",
-        },
-      ],
-    },
-    {
-      year: 2019,
-      experiences: [
-        {
-          type: sections.Projects,
-          title: "Asteroids Remake",
-          description: "Remake of the famous arcade game",
-          class: "asteroids",
-        },
-      ],
-    },
-  ];
+const experienceTimeline = [
+  {
+    year: 2023,
+    experiences: [
+      {
+        type: sections.Projects,
+        title: "Ecosquad",
+        description: "A citizen scientist web app",
+        class: "ecosquad",
+      },
+    ],
+  },
+  {
+    year: 2022,
+    experiences: [
+      {
+        type: sections.Career,
+        title: "Caseware",
+        description: "Software Developer Intern",
+        class: "caseware",
+      },
+    ],
+  },
+  {
+    year: 2021,
+    experiences: [
+      {
+        type: sections.Career,
+        title: "Skatescribe",
+        description: "Full Stack Developer Intern",
+        class: "skatescribe",
+      },
+    ],
+  },
+  {
+    year: 2019,
+    experiences: [
+      {
+        type: sections.Projects,
+        title: "Asteroids Remake",
+        description: "Remake of the famous arcade game",
+        class: "asteroids",
+      },
+    ],
+  },
+];
 
-  function createSidebar(activeSection) {
-    const sidebar = document.createElement("div");
-    sidebar.classList.add("sidebar");
-    sideBarContents.forEach((section) => {
-      const newSection = document.createElement("div");
-      newSection.classList.add("section");
-      newSection.innerHTML = `
+function createSidebar(activeSection) {
+  const sidebar = document.createElement("div");
+  sidebar.classList.add("sidebar");
+  sideBarContents.forEach((section) => {
+    const newSection = document.createElement("div");
+    newSection.classList.add("section");
+    newSection.innerHTML = `
         <div class="section-info">
           <div class="section-icon ${section.sectionClass}"></div>
           <div class="section-title">${section.sectionTitle}</div>
         </div>
         <div class="count">${section.sectionCount}</div>
       `;
-      sidebar.appendChild(newSection);
-    });
-    return sidebar;
-  }
+    sidebar.appendChild(newSection);
+  });
+  return sidebar;
+}
 
-  function createReminderContent(section) {
-    const reminderContent = document.createElement("div");
-    reminderContent.classList.add("reminders-content");
+function createReminderContent(section) {
+  const reminderContent = document.createElement("div");
+  reminderContent.classList.add("reminders-content");
 
-    const contentTitle = document.createElement("span");
-    contentTitle.classList.add("content-title");
-    contentTitle.innerHTML = section;
-    reminderContent.appendChild(contentTitle);
+  const contentTitle = document.createElement("span");
+  contentTitle.classList.add("content-title");
+  contentTitle.innerHTML = section;
+  reminderContent.appendChild(contentTitle);
 
-    const experienceTimelineElem = document.createElement("div");
-    experienceTimelineElem.classList.add("experience-timeline");
+  const experienceTimelineElem = document.createElement("div");
+  experienceTimelineElem.classList.add("experience-timeline");
 
-    experienceTimeline.forEach((yearExperienceMap) => {
-      const year = document.createElement("span");
-      year.classList.add("year");
-      year.innerHTML = yearExperienceMap.year;
-      experienceTimelineElem.appendChild(year);
+  experienceTimeline.forEach((yearExperienceMap) => {
+    const year = document.createElement("span");
+    year.classList.add("year");
+    year.innerHTML = yearExperienceMap.year;
+    experienceTimelineElem.appendChild(year);
 
-      yearExperienceMap.experiences.forEach((experience) => {
-        const experienceCard = document.createElement("div");
-        experienceCard.classList.add("experience-card", experience.class);
-        experienceCard.innerHTML = `
+    yearExperienceMap.experiences.forEach((experience) => {
+      const experienceCard = document.createElement("div");
+      experienceCard.classList.add("experience-card", experience.class);
+      experienceCard.innerHTML = `
           <div class="card-description">
             <span class="card-title">${experience.title}</span>
             <span>${experience.description}</span>
           </div>
         `;
-        experienceTimelineElem.appendChild(experienceCard);
-      });
-      const divider = document.createElement("div");
-      divider.classList.add("divider");
-      experienceTimelineElem.appendChild(divider);
+      experienceTimelineElem.appendChild(experienceCard);
     });
-    reminderContent.appendChild(experienceTimelineElem);
-    return reminderContent;
-  }
+    const divider = document.createElement("div");
+    divider.classList.add("divider");
+    experienceTimelineElem.appendChild(divider);
+  });
+  reminderContent.appendChild(experienceTimelineElem);
+  return reminderContent;
+}
 
-  function displayReminderWindow(section) {
-    const reminderApp = document.createElement("div");
-    reminderApp.id = "reminders-app";
+function displayReminderWindow(section) {
+  const reminderApp = document.createElement("div");
+  reminderApp.id = "reminders-app";
 
-    reminderApp.appendChild(createSidebar(section));
-    reminderApp.appendChild(createReminderContent(section));
+  reminderApp.appendChild(createSidebar(section));
+  reminderApp.appendChild(createReminderContent(section));
 
-    const reminderWindow = createWindow(reminderApp);
-    document.body.appendChild(reminderWindow);
-  }
-
-  displayReminderWindow("All Experiences");
-});
+  const reminderWindow = createWindow(reminderApp);
+  document.body.appendChild(reminderWindow);
+}

2457d4284b5f7718e3479bb6a2f3f643ad7918ef
Daniel Zhang <dwqz365@gmail.com>
2024-05-31 12:28:28 -0400

    Created new branch

--- resumeApp.js
+++
@@ -1,8 +0,0 @@
-function displayResumeAppWindow() {
-  const resume = document.createElement("iframe");
-  resume.src = "./files/Resume.pdf";
-  resume.width = "100%";
-  resume.height = "100%";
-  const reminderWindow = createWindow(resume);
-  document.body.appendChild(reminderWindow);
-}

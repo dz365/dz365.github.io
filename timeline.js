acb98a436d83175bc29565549c3748aaf2a1102e
Daniel Zhang <dwqz365@gmail.com>
2024-06-01 12:17:04 -0400

    Added event info

--- timeline.js
+++ timeline.js
@@ -1,7 +1,7 @@
 const events = [
   {
     type: "work",
-    title: "Service Delivery Data Hub Project",
+    title: "SDDH",
     class: ["sddh"],
   },
   {
@@ -25,6 +25,111 @@ const events = [
     class: ["asteroids"],
   },
 ];
+
+const workDescriptions = {
+  SDDH: {
+    company: "Service Delivery Data Hub Project",
+    position: "Full Stack Developer (Work Learning Experience)",
+    date: "November 2023",
+    description:
+      "My recent work was on the Service Delivery Data Hub project, a collaboration between \
+       Catholic Crosscultural Services (CCS), the University of Toronto Scarborough (UTSC), \
+       and the Toronto East Quadrant Local Immigration Partnership (TEQ LIP). The project \
+       aims to improve the settlement experiences of newcomers in Scarborough by providing \
+       evidence for better service delivery and planning. When I joined, essential functionalities \
+       were complete, and new features were being developed. My role involved integrating end-to-end \
+       tests into the build pipeline to ensure that new features did not disrupt existing functionality.",
+    technologies: ["Cypress", "JavaScript"],
+  },
+  Caseware: {
+    company: "Caseware International",
+    position: "Software Developer",
+    date: "January 2022 - August 2022",
+    description:
+      "My second internship was for 8 months at a medium sized auditing company called Caseware. \
+       At the time I was working, Caseware was in a transitional stage to fully move all their \
+       products to cloud providers. As such, most of what I worked on were related to this. Some \
+       of the major things I did include development of a routing microservice to handle incoming \
+       requests, and system improvements to better support client needs.",
+    technologies: ["Java", "Spring Boot", "AWS", "DynamoDB", "Redis", "C++"],
+  },
+  Skatescribe: {
+    company: "Skatescribe",
+    position: "Full Stack Developer",
+    date: "May 2021 - August 2021",
+    description:
+      "My first internship was for 4 months at a startup called Skatescribe. Backed by NHL investors, \
+       they are revolutionizing the skate sharpening industry by using machines, leading to high quality \
+       cuts that cannot be replicated anywhere else. What I worked on during my time here was developing \
+       measures to improve operational safety and automation to reduce the amount of interaction an \
+       operator has with the software.",
+    technologies: [
+      "Angular",
+      "Python",
+      "Python Flask",
+      "SocketIO",
+      "PostgreSQL",
+      "Electron",
+    ],
+  },
+};
+
+const projectDescriptions = {
+  Ecosquad: {
+    name: "Ecosquad",
+    link: "https://github.com/dz365/project-ecosquad",
+    description:
+      "Ecosquad is a web app developed by me and a friend of mine. The inspiration behind it was \
+       that we wanted a place where we could upload wildlife discoveries that we would make from \
+       time to time. Hence we made a citizen scientist app where users could share, search, and \
+       view discoveries on an interactive map.",
+    technologies: [
+      "React",
+      "MapLibre GL",
+      "ExpressJS",
+      "PostgreSQL",
+      "Meilisearch",
+      "Docker",
+    ],
+  },
+  Asteroids: {
+    name: "Asteroids Remake",
+    link: "https://github.com/dz365/Asteroid-Remake",
+    description:
+      "This was a project that I did along with a friend quite a while back, but I had so much \
+       fun creating that I decided to include it here. It's also nice to be reminded of how far \
+       I've come in such a short period of time. As per the name, this was a remake of Asteroids \
+       by Altari, alongisde additional features like powerups.",
+    technologies: ["Java", "Java Swing"],
+  },
+};
+
+function updateInfoBodyWithWork(workName) {
+  const workInfo = workDescriptions[workName];
+  document.querySelector(".info-body").innerHTML = `
+    <p>${workInfo.company}</p>
+    <p>${workInfo.position}</p>
+    <p>${workInfo.date}</p>
+    <p>${workInfo.description}</p>
+    <p>${workInfo.technologies}</p>
+  `;
+}
+
+function updateInfoBodyWithProject(projectName) {
+  const projectInfo = projectDescriptions[projectName];
+  document.querySelector(".info-body").innerHTML = `
+    <p>${projectInfo.name}</p>
+    <p>${projectInfo.link}</p>
+    <p>${projectInfo.description}</p>
+    <p>${projectInfo.technologies}</p>
+  `;
+}
+
+function updateInfoBody(eventTitle, eventType) {
+  if (eventType === "work") updateInfoBodyWithWork(eventTitle);
+  else if (eventType === "project") updateInfoBodyWithProject(eventTitle);
+}
+
 window.addEventListener("DOMContentLoaded", function () {
   const timeline = document.querySelector(".timeline");
 
@@ -34,7 +139,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
     eventElem.addEventListener("positionChange", () => {
       if (Math.abs(getCenter(eventElem).x - window.innerWidth / 2) < 100)
-        document.querySelector(".info-body").innerHTML = event.title;
+        updateInfoBody(event.title, event.type);
     });
 
     timeline.appendChild(eventElem);

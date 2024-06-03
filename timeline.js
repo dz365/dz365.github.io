1678f540511b45ee2d1fd8e881ea0c6bb6e6afae
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2024-06-03 12:40:42 -0400

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -293,6 +293,6 @@ window.addEventListener("DOMContentLoaded", function () {
     touchCurrentX = e.touches[0].clientX;
     const touchEndX = e.changedTouches[0].clientX;
     const deltaX = touchEndX - touchStartX;
-    updateTimelinePosition(-deltaX / 10);
+    updateTimelinePosition(-deltaX / 8);
   });
 });
